49b5a13302ee55d1c379b2269b1f4b4bc3f0a3d4
morch028 <morch028@uottawa.ca>
2018-08-12 19:17:31 -0400

    Adds more descriptive startup error messages
    Now all messages that prevent startup will cause the app to exit, and
    will provide more detail on what caused the failure.

--- index.js
+++ index.js
@@ -2,20 +2,37 @@ const BotKit = require("botkit");
 const BotkitStorage = require('botkit-storage-mongo');
 const SlashBus = require("./behaviors/slash-bus");
 
-if (!process.env.CLIENT_ID
-  || !process.env.CLIENT_SECRET
-  || !process.env.PORT
-  || !process.env.REDIRECT_URI
-  || !process.env.MONGOLAB_URI
-  || !process.env.OAUTH_SCOPES
-  || !process.env.VERIFICATION_TOKEN) {
-  console.log('Error: Specify all necessary environment variables (7)');
+function startupError(error) {
+  console.error(error);
   process.exit(1);
 }
+
+const requiredEnvVariables = [
+  // For hosting
+  "PORT",
+  // For Slack
+  "CLIENT_ID",
+  "CLIENT_SECRET",
+  "VERIFICATION_TOKEN",
+  "OAUTH_SCOPES",
+  "REDIRECT_URI",
+  // For MongoLab
+  "MONGOLAB_URI",
+  //For OCTranspo
+  "OCTRANSPO_APP_ID",
+  "OCTRANSPO_API_KEY"
+];
+
+const missingEnvVariables = requiredEnvVariables.filter(variable => !process.env[variable]);
+
+if (missingEnvVariables.length > 0) {
+  startupError(`Error, missing: ${missingEnvVariables} in environment variables`);
+}
+
 const botkitConfig = {
   storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
 };
-
+console.log(botkitConfig.storage);
 const controller = BotKit.slackbot(botkitConfig).configureSlackApp({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
@@ -23,15 +40,15 @@ const controller = BotKit.slackbot(botkitConfig).configureSlackApp({
   scopes: process.env.OAUTH_SCOPES.split(",")
 });
 
-controller.setupWebserver(process.env.PORT, (error, webserver) => {
-  if (error) {
-    console.error("Unexpected error occured while setting up webserver", error);
-    return;
+controller.setupWebserver(process.env.PORT, setupError => {
+  if (setupError) {
+    startupError(setupError);
   }
   controller.createWebhookEndpoints(controller.webserver);
-  controller.createOauthEndpoints(controller.webserver, (err, req, res) => {
-    if (err) {
-      res.status(500).send('ERROR: ' + err);
+  controller.createOauthEndpoints(controller.webserver, (oauthError, req, res) => {
+    if (oauthError) {
+      res.status(500).send(oauthError);
+      startupError("unable to serve OAuth Endpoints", oauthError);
     } else {
       res.send('Success!');
     }
