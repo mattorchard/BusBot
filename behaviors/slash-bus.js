4e510440258b0b5fcdee0bb2f4d86b2180e2d728
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-18 10:48:23 -0400

    Restructure controller so it can await OCT data.

--- behaviors/slash-bus.js
+++ behaviors/slash-bus.js
@@ -2,29 +2,22 @@ const oct = require('./../services/octranspo-fetch-service.js');
 
 module.exports = function (controller) {
   console.log("Attaching /bus behaviors");
-
-    controller.on('slash_command', (bot, message) => {
+    
+    async function asyncreply(bot,message){
       console.log("Message received!", new Date().getTime());
       switch(message.command) {
         case "/nextbus":
         case "/stopinfo":
-          console.log("[slash-bus] Message recieved, '"+message+"', replying.");
-          bot.replyPrivate(message, "Grabbing bus info for stop 7659...");
-          asyncreply(bot,message);
+          console.log("[slash-bus] Replying when asynchronous data is returned.");
+          octData = await oct.nextBus(7659);
+          bot.replyPrivate(bot,octData);
           break;
         default:
           bot.replyPrivate(message, `What does: \`${message.text}\` mean?`);
           break;
       }
       console.log("[slash-bus] Message sent!", new Date().getTime());
+    }
 
-    });
-
+    controller.on('slash_command', asyncreply(bot, message));
 };
-
-async function asyncreply(bot,message){
-          console.log("[slash-bus] Replying when asynchronous data is returned.");
-          octData = await oct.nextBus(7659);
-          bot.replyPrivate(bot,octData);
-          console.log("[slash-bus] Sent async message.");
-}
