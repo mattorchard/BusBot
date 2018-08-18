7d28d9394937d109d4c5eba814fc69470c283cd6
Ryan Fleck <ryan.fleck@protonmail.com>
2018-08-18 15:59:07 -0400

    Add error messages for users.

--- services/octranspo-fetch-service.js
+++ services/octranspo-fetch-service.js
@@ -1,13 +1,5 @@
 const fetch = require('node-fetch');
-
-//API and APP keys now utilized in octranspo-service.js
 const util = require('./octranspo-link-service.js');
-
-// nextBus() and stopInfo() return JSON in the form:
-// {
-//   "text": "Info\nMoreInfo"
-// }
-
 
 async function fetchData (stop,route,direction) {
 
@@ -16,39 +8,34 @@ async function fetchData (stop,route,direction) {
   if(route){postData.routeNo = route;}
   if(direction){postData.direction = direction;}
   
-  // Check cached data.
-
   const response =  await fetch(util.getUrl("GetNextTripsForStopAllRoutes",postData)); 
   const busData = await response.json(); 
-  //console.log("[oct-fetch]\nRaw OC-Transpo Data:\n\n"+JSON.stringify(busData.GetRouteSummaryForStopResult,null,2)+"\n");
   return busData.GetRouteSummaryForStopResult;
 }
-
 
 async function nextBus (stop, route=null, direction=null) {
 
   return stopInfo(stop, route, direction);//Temporary: Direct all input to stopInfo.
-  
+  /*  
   console.log("[oct-fetch] nextBus("+stop+" "+route+" "+direction+")");
 
   if(!route){
     console.log("[oct-fetch] no route data provided, returning stopinfo."); 
     return stopInfo(stop);
-  }
+  }*/
 }
-
 
 async function stopInfo (stop, route=null, direction=null) {
   
   const output = [];
+  let route_present=false;
   
   const busData = await fetchData(stop).catch(function(err) {
       console.log('Error: '+err);
       });
 
-  //console.log(busData);
   output.push("Connections at stop *"+busData.StopNo+"*, "+busData.StopDescription+":");
-  console.log( "Is this an array? >>" + Array.isArray(busData.Routes.Route) )
+  
   if( Array.isArray(busData.Routes.Route)){ 
     for(busNum in busData.Routes.Route){
       const Route = busData.Routes.Route[busNum];
@@ -57,6 +44,7 @@ async function stopInfo (stop, route=null, direction=null) {
         const Trip = Route.Trips[tripNum];
         TripArray.push(Trip.TripStartTime);
       }
+      if(Route.RouteNo==route){route_present=true;}
       if(route==null || Route.RouteNo==route){ 
         output.push("*"+Route.RouteNo+"* "+Route.Direction+" to "+Route.RouteHeading+" at "+
         TripArray.join(", ")+".");
@@ -69,18 +57,31 @@ async function stopInfo (stop, route=null, direction=null) {
       const Trip = Route.Trips.Trip[tripNum];
       TripArray.push(Trip.TripStartTime);
     }
+    if(Route.RouteNo==route){route_present=true;}
     if(route==null || Route.RouteNo==route){ 
       output.push("*"+Route.RouteNo+"* "+Route.Direction+" to "+Route.RouteHeading+" at "+
       TripArray.join(", ")+".");
     }
   }
-
+ 
+  if(output.length==1){
+    switch(route){
+      case null:
+        output.push("No connecting routes found.");
+        break;
+      default:
+        output.push("Route "+route+" does not pass stop "+stop+" at this time.");
+        //Separate output-building into another method so all routes can be repeated here.
+        break; 
+    }
+  }
+  
   jsonOut = { "text" : output.join("\n") };
   console.log("\nReplying with Slack Message:\n\n"+JSON.stringify(jsonOut,null,2)+"\n");
   return jsonOut;  
 }
 
-console.log(nextBus(3010,95).text);
+console.log(nextBus(3010,16).text);
 //console.log(stopInfo(3010).text);
 
 module.exports = { nextBus, stopInfo };
