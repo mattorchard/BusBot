b4127e62fc46552a74bd399651266785281af3f2
morch028 <morch028@uottawa.ca>
2018-09-12 07:46:33 -0400

    Move OCTranspo constants into separate file

--- services/octranspo-link-service.js
+++ services/octranspo-link-service.js
@@ -1,22 +1,22 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}
+const octranspoConstants = require("./octranspo-constants.json");
 
-const BASE_URL = "https://api.octranspo1.com/v1.2";
 const BASE_QUERY_PARAMS = {
   format: "json",
-  appId: process.env.OCTRANSPO_APP_ID,
+  appID: process.env.OCTRANSPO_APP_ID,
   apiKey: process.env.OCTRANSPO_API_KEY
 };
 
 function getUrl(endpoint, queryParams) {
+  const fullEndpoint = octranspoConstants.endpoints[endpoint];
+  if (!fullEndpoint) {throw new Error(`Invalid endpoint [${endpoint}]`);}
+
   const queryEntries = Object.entries(Object.assign(BASE_QUERY_PARAMS, queryParams));
   const populatedQueryEntries = queryEntries.filter(entry => entry[0] && entry[1]);
 
   const queryList = populatedQueryEntries.map(entry => `${entry[0]}=${entry[1]}`);
   const queryString = queryList.join("&");
 
-  return `${BASE_URL}/${endpoint}?${queryString}`;
+  return `${octranspoConstants.baseUrl}/${fullEndpoint}?${queryString}`;
 }
 
 module.exports = { getUrl };
--- tests/octranspo-link-service.test.js
+++ tests/octranspo-link-service.test.js
@@ -1,21 +1,21 @@
 const octranspoLinkService = require('../services/octranspo-link-service');
 
 test('url should include default query parameters', () =>{
-  const url = octranspoLinkService.getUrl();
+  const url = octranspoLinkService.getUrl("stopInfo");
   expect(url).toContain("format=json");
 });
 
 test('url should include endpoint', () => {
-  const url = octranspoLinkService.getUrl("TestEndpoint");
-  expect(url).toContain("TestEndpoint");
+  const url = octranspoLinkService.getUrl("stopInfo");
+  expect(url).toContain("GetNextTripsForStopAllRoutes");
 });
 
 test('url should include custom query parameters', () => {
-  const url = octranspoLinkService.getUrl("TestEndpoint", {'key': 'value'});
+  const url = octranspoLinkService.getUrl("stopInfo", {'key': 'value'});
   expect(url).toContain("key=value");
 });
 
 test('url should not include falsey parameters', () => {
-  const url = octranspoLinkService.getUrl("TestEndpoint", {'key': ''});
+  const url = octranspoLinkService.getUrl("stopInfo", {'key': ''});
   expect(url).not.toContain("key");
 });
