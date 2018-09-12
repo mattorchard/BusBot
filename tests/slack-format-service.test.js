40800c5c04de4a40dfa33d3602cede4cf16054a2
Matthew <morch028@uottawa.ca>
2018-09-12 13:53:35 -0400

    Rename method call in tests to reflect recent refactor

--- tests/slack-format-service.test.js
+++ tests/slack-format-service.test.js
@@ -6,19 +6,19 @@ const mockOctranspo = require('./mock-octranspo-api');
 beforeEach(mockOctranspo);
 
 test('Should contain stop name', async() => {
-  const stopInfo = await octranspoService.stopInfo(3022);
+  const stopInfo = await octranspoService.getStopInfo(3022);
   const reply = slackFormatService.formatStopInfo(stopInfo);
   expect(reply).toContain("*LEES");
 });
 
 test('Should contain route name', async() => {
-  const stopInfo = await octranspoService.stopInfo(3022);
+  const stopInfo = await octranspoService.getStopInfo(3022);
   const reply = slackFormatService.formatStopInfo(stopInfo);
   expect(reply).toContain("*101 Moodie");
 });
 
 test('Should contain route name of filtered route', async() => {
-  const stopInfo = await octranspoService.stopInfo(3022, 101);
+  const stopInfo = await octranspoService.getStopInfo(3022, 101);
   const reply = slackFormatService.formatStopInfo(stopInfo);
   expect(reply).toContain("*101 Moodie");
 });
